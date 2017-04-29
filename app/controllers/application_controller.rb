c92774a2159e0f37a74a39aff366ec2259956932
Tsukasa Watanabe <tswata@gmail.com>
2017-04-29 05:46:21 +0000

    want

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,4 +10,22 @@ class ApplicationController < ActionController::Base
       redirect_to login_url
       end
   end
+  
+  
+  def read(result)
+    code = result.code
+    name = result.name
+    #name = result["itemName"]
+    #url = result.url
+    url = result["itemUrl"]
+    image_url = result["mediumImageUrls"].first["imageUrl"].gsub("?_ex=128x128", "")
+  
+   return{
+     code: code,
+     name: name,
+     url: url,
+     image_url: image_url,
+   }
+  end
+  
 end
